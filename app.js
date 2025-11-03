46c749ee5b8bd182a332bbd3c853b1eaa7978322
huma515 <humahashim11@gmail.com>
2025-11-03 21:57:32 +0500

    map method practice

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
-let map = new map (()=>{
+
+// let newmap = new Map()
+// newmap.set("name", "huma")
+// newmap.set("father", "hashim")
+// newmap.set("age", 21)
+// newmap.set("city", "karachi")
+// newmap.delete("name")
+// // newmap.clear()
+// newmap.entries()
+// console.log(newmap.get("age"))
+// console.log(newmap, typeof newmap)
+
+
+// newmap.forEach(function(key , value){
+
+// console.log(value+ " " +key)
+
+// })
+
+// for(const i of newmap){
+
+//     console.log(i)
+// }
 
 
 
 
 
 
-    
-})
+
+
+
+
+
+
+
+
+
+
+
+
